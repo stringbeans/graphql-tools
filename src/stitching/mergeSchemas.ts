1e3bb14360e037ff05b8767565ce1c4f8bb99628
Mikhail Novikov <freiksenet@gmail.com>
2017-12-19 14:06:13 +0200

    Check null resolvers

--- src/stitching/mergeSchemas.ts
+++ src/stitching/mergeSchemas.ts
@@ -56,6 +56,10 @@ export default function mergeSchemas({
   const extensions: Array<DocumentNode> = [];
   const fragments = {};
 
+  if (!resolvers) {
+    resolvers = {};
+  }
+
   if (!visitType) {
     visitType = defaultVisitType;
   }
